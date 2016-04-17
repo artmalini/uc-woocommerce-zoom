aeb79eb676d59554eed8c495c3d10e6511ef4f7e
Artem <artmalini@gmail.com>
2016-04-17 20:51:18 +0300

    first commit

---
+++ uc-woocommerce-zoom.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       UC Woocommerce Zoom
+ * Plugin URI:        https://github.com/artmalini/uc-woocommerce-zoom.git
+ * Description:       Awesome plugin for woocommerce product zoom.
+ * Version:           1.0
+ * Author:            Artem Makhinya
+ * Author URI:        https://github.com/artmalini/uc-woocommerce-zoom.git
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       uc-woocommerce-zoom
+ * Domain Path:       /languages
+ */
+
+
+
+
+
+
+?>
