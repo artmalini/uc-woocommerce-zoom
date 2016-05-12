d627a866ddcc24aff253d9ed52a10ebd25ef20b6
Artem <artmalini@gmail.com>
2016-05-12 13:17:55 +0300

    'as12'

--- uc-woocommerce-zoom.php
+++ uc-woocommerce-zoom.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name:       UC Woocommerce Zoom
  * Plugin URI:        https://github.com/artmalini/uc-woocommerce-zoom.git
- * Description:       Awesome plugin for woocommerce product zoom.
- * Version:           1.0
+ * Description:       My Awesome plugin for woocommerce product zoom.
+ * Version:           1.1
  * Author:            Artem Makhinya
  * Author URI:        https://github.com/artmalini/uc-woocommerce-zoom.git
  * License:           GPL-2.0+
@@ -12,9 +12,30 @@
  * Domain Path:       /languages
  */
 
+/**
+ * Define Path 
+ */
+
+define( 'UC_W_ZOOM', WP_CONTENT_URL. '/plugins/uc-woocommerce-zoom' );
 
 
 
+/**
+ * Require Files
+ */
+
+require_once dirname( __FILE__ ) . '/includes/uc-settings.php';
+
+/**
+ * Adding scripts
+ */
+
+if( !function_exists('uc_adding_scripts') ):
+	function uc_adding_scripts() {		
+		wp_enqueue_script('uc-zoomsl', UC_W_ZOOM.'/js/zoomsl-3.0.js', array('jquery'),'3.0', false);
+	}
+endif;
+add_action( 'wp_enqueue_scripts', 'uc_adding_scripts' ); 
 
 
 ?>
